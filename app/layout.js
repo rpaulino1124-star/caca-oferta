640560580eabb7bfcf89f72d33887f4b0feb2d45
rpaulino1124-star <rpaulino1124@gmail.com>
2026-09-25 14:52:33 -0300

    Adicionar app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,1 @@
+import "./globals.css"; export const metadata={title:"Caça Oferta",description:"Painel de automação de ofertas"}; export default function RootLayout({children}){return <html lang="pt-BR"><body>{children}</body></html>}
